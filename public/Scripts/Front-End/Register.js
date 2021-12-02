4822a5f526f2df77451ec5c0106be9a824fda2b0
Darkness4869 <andygaspard@hotmail.com>
2021-12-02 22:08:47 +0400

    Update 1.18
    Adding the back-end of the registration page

--- public/Scripts/Front-End/Register.js
+++ public/Scripts/Front-End/Register.js
@@ -69,7 +69,7 @@ class Main extends React.Component {
     // Submit handler method
     handleSubmit(event) {
         // Local variables
-        const delay = 1560;
+        const delay = 2800;
         // Prevent default submission
         event.preventDefault();
         // Generating a POST request
