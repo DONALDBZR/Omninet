5decb6e777861a714a8a615176125d3c365f712b
Darkness4869 <andygaspard@hotmail.com>
2021-12-03 22:00:25 +0400

    Update 1.25
    Adding the main for the seller's page

--- public/Scripts/Front-End/Seller.js
+++ public/Scripts/Front-End/Seller.js
@@ -31,9 +31,97 @@ class Header extends React.Component {
 }
 // Main class
 class Main extends React.Component {
+    // Constructor method
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            image: "",
+            success: "",
+            message: "",
+            url: "",
+        };
+    }
+    // Change handler method
+    handleChange(event) {
+        // Local variables
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        // Setting the value
+        this.setState({
+            [name]: value,
+        });
+    }
+    // Submit handler method
+    handleSubmit(event) {
+        // Local variables
+        const delay = 1560;
+        // Prevent default submission
+        event.preventDefault();
+        // Generating a POST request
+        fetch("./Seller.php", {
+            method: "POST",
+            body: JSON.stringify({
+                name: this.state.name,
+                image: this.state.image,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((response) => response.json())
+            .then((data) =>
+                this.setState({
+                    success: data.success,
+                    message: data.message,
+                    url: data.url,
+                })
+            )
+            .then(() => this.redirector(delay));
+    }
+    // Redirector method
+    redirector(delay) {
+        setTimeout(() => {
+            window.location.href = this.state.url;
+        }, delay);
+    }
     // Render method
     render() {
-        return <main></main>;
+        return (
+            <main>
+                <form method="POST" enctype="multipart/form-data">
+                    <div id="label">Adding items</div>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Name"
+                        value={this.state.name}
+                        onChange={this.handleChange.bind(this)}
+                        required
+                    />
+                    <label class="fas fa-upload faUpload">
+                        <input
+                            type="file"
+                            name="image"
+                            accept="image/*"
+                            value={this.state.image}
+                            onChange={this.handleChange.bind(this)}
+                            required
+                        />
+                    </label>
+                    <div id="addButton">
+                        <button>Add</button>
+                    </div>
+                </form>
+                <div id="serverRendering">
+                    <div id="addForm">
+                        <h1 id={this.state.success}>{this.state.message}</h1>
+                    </div>
+                    <div id="data"></div>
+                </div>
+            </main>
+        );
     }
 }
 // Footer class
