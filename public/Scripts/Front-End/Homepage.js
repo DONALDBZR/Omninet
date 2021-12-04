1633e5ddb16a1ccfe8fd98a065dce8f11e2c102b
Darkness4869 <andygaspard@hotmail.com>
2021-12-04 23:47:02 +0400

    Update 1.33
    Modification of the header of the Homepage

--- public/Scripts/Front-End/Homepage.js
+++ public/Scripts/Front-End/Homepage.js
@@ -14,26 +14,15 @@ class Header extends React.Component {
                 <div id="logo">
                     <a href="./">Omninet</a>
                 </div>
-                <nav id="catalog">
-                    <div>
-                        <a href="./Shop">Shops</a>
-                    </div>
-                    <div>
-                        <a href="./Brand">Brands</a>
-                    </div>
-                </nav>
-                <nav id="utilities">
-                    <div>
-                        <a href="./User" class="fas fa-user faUser"></a>
-                    </div>
-                    <div>
-                        <a href="./Search" class="fas fa-search faSearch"></a>
-                    </div>
+                <nav>
                     <div>
                         <a
-                            href="./Cart"
+                            href="./Shop"
                             class="fas fa-shopping-cart faCart"
                         ></a>
+                    </div>
+                    <div>
+                        <a href="./User" class="fas fa-user faUser"></a>
                     </div>
                 </nav>
             </header>
